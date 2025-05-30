cfd28ddae52e08d697179038130fae8c4b4a1869
KMZ7878 <marqueskaue771@gmail.com>
2025-05-30 18:48:40 -0300

    Initial commit

---
+++ src/main/java/dev/kmz/FirstCRUDSpring/FirstCrudSpringApplication.java
@@ -0,0 +1,13 @@
+package dev.kmz.FirstCRUDSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstCrudSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstCrudSpringApplication.class, args);
+	}
+
+}
